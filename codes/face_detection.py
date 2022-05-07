3d610a851a6e1bcaf24f4a4bdb158338a390e78f
thegenetic <pauldipesh29@gmail.com>
2022-05-07 19:51:15 +0530

    edited scripts

--- codes/face_detection.py
+++ codes/face_detection.py
@@ -1,19 +1,34 @@
 import cv2
+from tkinter import filedialog
+from tkinter import *
+
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# created an instance of tkinter 
+root = Tk()
+# deleting the default window provided by tkinter
+root.withdraw()
+filepath = filedialog.askopenfilename(title="select image")
 # Read the input image
-img = cv2.imread('face.jpg')
+img = cv2.imread(filepath)
 
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+
 # Detect faces
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+# uniquely name each detected faces using count variable
+count = 0
+
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
+    face = img[y:y+h, x:x+w] #slice the face from the image
+    cv2.imwrite(str(count)+'.jpg', face) #save the image
+    count+=1
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 2)
 
 # Display the output
